9e3bcf2c38c6ac08eec1e117810544b91c7a1bef
Han Verstraete <welteki@pm.me>
2021-06-05 09:37:13 +0200

    Include home.nix
    Include home.nix from nix-home: https://github.com/welteki/nix-home

---
+++ home.nix
@@ -0,0 +1,134 @@
+{ config, pkgs, lib, ... }:
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "han";
+  home.homeDirectory = if pkgs.stdenv.isDarwin then "/Users/han" else "/home/han";
+
+  home.packages = with pkgs; [] ++ lib.optionals stdenv.isDarwin [
+    # Ensure at least bash v4 on macOS for zsh-nix-shell - https://github.com/chisui/zsh-nix-shell/issues/14
+    bash
+  ];
+
+  programs = {
+    git = {
+      enable = true;
+      userName = "Han Verstraete";
+      userEmail = "welteki@pm.me";
+      aliases = {
+        co = "checkout";
+        ci = "commit";
+        cia = "commit --amend";
+        st = "status";
+        b = "branch";
+        pu = "push";
+        mff = "merge --ff-only";
+      };
+      ignores = [
+        # macOS.gitignore source:https://github.com/github/gitignore/blob/master/Global/macOS.gitignore
+        # General
+        ".DS_Store"
+        ".AppleDouble"
+        ".LSOverride"
+
+        # Thumbnails
+        "._*"
+      ];
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+    };
+    
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      enableAutosuggestions = true;
+      plugins = [
+        {
+          name = "zsh-syntax-highlighting";
+          file = "zsh-syntax-highlighting.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "zsh-users";
+            repo = "zsh-syntax-highlighting";
+            rev = "0.7.1";
+            sha256 = "03r6hpb5fy4yaakqm3lbf4xcvd408r44jgpv4lnzl9asp4sb9qc0";
+          };
+        }
+        {
+          name = "zsh-nix-shell";
+          file = "nix-shell.plugin.zsh";
+          src = pkgs.fetchFromGitHub {
+            owner = "chisui";
+            repo = "zsh-nix-shell";
+            rev = "v0.2.0";
+            sha256 = "1gfyrgn23zpwv1vj37gf28hf5z0ka0w5qm6286a7qixwv7ijnrx9";
+          };
+        }
+      ];
+      initExtra = ''
+        if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then
+          . ~/.nix-profile/etc/profile.d/nix.sh;
+        fi # added by Nix installer
+        export NIX_PATH=$HOME/.nix-defexpr/channels''${NIX_PATH:+:}$NIX_PATH
+        '';
+    };
+
+    direnv = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
+    starship = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+        docker_context = {
+          symbol = " ";
+        };
+        git_branch = {
+          symbol = " ";
+        };
+        golang = {
+          symbol = " ";
+        };
+        java = {
+          symbol = " ";
+        };
+        kubernetes = {
+          symbol = "ﴱ ";
+        };
+        nix_shell = {
+          symbol = " ";
+        };
+        nodejs = {
+          symbol = " ";
+        };
+        package = {
+          symbol = " ";
+        };
+        python = {
+          symbol = " ";
+        };
+        terraform = {
+          symbol = "  ";
+        };
+      };
+    };
+
+    bat.enable = true;
+    jq.enable = true;
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.05";
+}
